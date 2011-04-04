3a164f6a26e651ca3b04f4132f0387a9975431a2
Sean Hayes <sean@seanhayes.name>
2011-04-03 23:45:46 -0400

    worked around caveat that prevents DB from being queried before tests are run in constants.py.

--- django_facebook_oauth/constants.py
+++ django_facebook_oauth/constants.py
@@ -1,7 +1,16 @@
 from django.contrib.sites.models import Site
 from django.conf import settings
 
+_host = None
+
+def _get_host_from_db():
+	"Used to get around the fact that the DB isn't set up during testing when this is first called."
+	if _host is None:
+		_host = Site.objects.get_current().domain
+	
+	return _host
+
 if hasattr(settings, 'HOST'):
 	HOST = settings.HOST
 else:
-	HOST = Site.objects.get_current().domain
+	HOST = property(_get_host_from_db)
